b107642057912b7251ccd8edaf22cca17ce7cb7c
rk-conway <ronoconway19@gmail.com>
2025-02-03 02:03:17 +0300

    made function to store to local storage

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+const BASE_URL = `https://dog.ceo/api/breeds/image/random`;
+let dog = null;
+let dogArr = [];
+
 async  function getData() {
     try{
-        const res = await fetch(`https://dog.ceo/api/breeds/image/random`);
+        const res = await fetch(`${BASE_URL}`);
         const image = await res.json();
         console.log(image);
         
         if(image){
             document.querySelector('.card').style.background = `url(${image.message}) no-repeat center/cover`;
-            document.querySelector('img').src = image.message;
+            // document.querySelector('img').src = image.message;
+            dog = image;
         }
 
         if(!res.status){
@@ -22,3 +27,23 @@ async  function getData() {
 }
 
 getData();
+
+
+function addLocalStorage(newDog){
+    if(!dog) return;
+    
+    const localDogArr = localStorage.getItem("dogArray");
+
+    if(localDogArr){
+        let newDogArr = JSON.parse(localDogArr);
+        console.log("the current dog array:",newDogArr);
+        newDogArr.push(newDog);
+        localStorage.setItem("dogArray",JSON.stringify(newDogArr));
+
+    }else{
+        let objDogArr = [];
+        objDogArr.push(newDog);
+        console.log("this is the initialized arr: ",objDogArr);
+        localStorage.setItem("dogArray",JSON.stringify(objDogArr));
+    }
+}
